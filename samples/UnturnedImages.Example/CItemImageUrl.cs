0280f099a3516bbcf9a829a7ca46e095c59925dd
Stephen White <iamsilk3495@gmail.com>
2021-09-08 16:00:16 -0300

    Added UnturnedImages.Example sample plugin

---
+++ samples/UnturnedImages.Example/CItemImageUrl.cs
@@ -0,0 +1,48 @@
+﻿using Cysharp.Threading.Tasks;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Commands;
+using OpenMod.Unturned.Users;
+using System;
+using System.Drawing;
+using UnturnedImages.API.Items;
+
+namespace UnturnedImages.Example
+{
+    [Command("itemimageurl")]
+    [CommandSyntax("<item ID>")]
+    [CommandDescription("Gets the image URL for an item asset.")]
+    public class CItemImageUrl : UnturnedCommand
+    {
+        private readonly IItemImageDirectoryAsync _itemImageDirectory;
+
+        public CItemImageUrl(IItemImageDirectoryAsync itemImageDirectory,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            _itemImageDirectory = itemImageDirectory;
+        }
+
+
+        protected override async UniTask OnExecuteAsync()
+        {
+            var itemId = await Context.Parameters.GetAsync<ushort>(0);
+
+            var itemUrl = await _itemImageDirectory.GetItemImageUrlAsync(itemId);
+
+            if (itemUrl == null)
+            {
+                await PrintAsync($"The image URL for item ID {itemId} could not be found.", Color.Red);
+
+                return;
+            }
+
+            await PrintAsync($"The image URL for item ID {itemId} is {itemUrl}");
+
+            if (Context.Actor is UnturnedUser user)
+            {
+                await UniTask.SwitchToMainThread();
+
+                user.Player.Player.sendBrowserRequest($"The image for item ID {itemId}", itemUrl);
+            }
+        }
+    }
+}
---
+++ samples/UnturnedImages.Example/UnturnedImagesExamplePlugin.cs
@@ -0,0 +1,18 @@
+﻿extern alias JetBrainsAnnotations;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+using System;
+
+[assembly: PluginMetadata("UnturnedImages.Example", DisplayName = "Unturned Images Example")]
+
+namespace UnturnedImages.Example
+{
+    [UsedImplicitly]
+    public class UnturnedImagesExamplePlugin : OpenModUnturnedPlugin
+    {
+        public UnturnedImagesExamplePlugin(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
